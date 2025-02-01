ae67a58835b6fb3799b38345d43a6cb264370abc
Belal Elkhateeb <belalelmansy796@gmail.com>
2025-02-02 00:39:58 +0200

    add Seqeunce App file

--- Seqeunce App/app.cpp
+++ Seqeunce App/app.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <array>
 using namespace std;
@@ -6,46 +5,107 @@ using namespace std;
 int main()
 
 {
-  int points = 0;
-  int answers[3];
+// int n1,n2;
+// int op;
+// cout << "Type Your First Number"<<endl;
+// cin >> n1;
+// cout <<"Choose your operation"<<endl;
+// cout <<"[1] + "<<endl;
+// cout <<"[2] - "<<endl;
+// cout <<"[3] * "<<endl;
+// cout <<"[4] / "<<endl;
+// cin >>op;
+// cout << "Type Your second Number"<<endl;
+// cin >> n2;
+// switch (op)
+// {
+// case 1:
+//     cout << " n1 " << " + " <<" n2 "<< " = "<< n1+n2 ;
+//     break;
+// case 2:
+//     cout << " n1 " << " - " <<" n2 "<< " = "<< n1-n2 ;
+//     break;
+// case 3:
+//     cout << " n1 " << " * " <<" n2 "<< " = "<< n1*n2 ;
+//     break;
+// case 4:
+//     cout << " n1 "  << " / "<< " n2 "<< " = " <<  n1/n2 ;
+//     break;
+
+// default:
+// cout <<"Operation Is Not Valid "<< endl;
+
+// }
+
+
+// int nums [] = {100,200,300,500};
+// cout << "First Num is "<< nums[0]<<endl;
+// cout << "location "<< &nums[0]<<endl;
+
+  // int nums[]= {100 ,200 ,250 ,300 ,400,300 ,400,300 ,400};
+
+  // cout << "Nums count is " << sizeof (nums) / sizeof (nums[0]); // TOTAL SIZE FOR ELEMENTS / SIZE OF ONE ELEMENT = 36 / 4 = 9 ELEMENTS
+
+  // int points_a [3]={1,2,3};
+  // int points_b [3]={4,5,6};
+  // int points_c [3]={7,8,9};
+  // const int rows= 3;
+  // const int column= 3;
+
+  // int points [rows][column]={{1,2,3},{4,5,6},{7,8,9}};
+  // cout << points[1][1]<<endl;
+  // cout << points[2][2]<<endl;
+  // cout << points[0][1]<<endl;
+  // cout << points[2][1]<<endl;
+  // cout << points[1][0]<<endl;
   
-  cout <<"Type The MIssing Num OF Sequence"<<endl;
+  // array<int,5> nums = {1,2,3,4,5};
+  // cout <<nums[0]<<endl;
+  // cout <<nums[1]<<endl;
+  // cout <<nums[2]<<endl;
+  // cout <<nums[3]<<endl;
+  // cout <<nums[4]<<endl;
+  // cout <<"Elements Count = "<< nums.size()<<endl;
   
-   cout <<" The First Sequence  "<<endl;
-   cout <<" 1 | 2 | 3| 5| ?? "<<endl;
-   cin >> answers[0];
+  // array<int,5> nums = {100,200,300,400,500};
+  // cout << nums[0]<<endl;
+  // cout << nums.front()<<endl;// first element (front)
+  // cout << nums.back()<<endl;// last element(back)
+  // cout << nums.at(2)<<endl;
+  // cout << nums.size()<<endl;
+  // int points = 0;
+//  int answers[3];
 
-   cout <<" The  Second Sequence "<<endl;
-   cout <<" 2 | 4 | 8 | 16| ?? "<<endl;
-   cin >> answers[1];
+//   cout <<"Type The MIssing Num OF Sequence"<<endl;
+//    cout <<" The  Sequence one "<<endl;
+//    cout <<" 1 | 2 | 3| 5| ?? "<<endl;
+//    cin >> answers[0];
+//    cout <<" The  Second Sequence "<<endl;
+//    cout <<" 2 | 4 | 8 | 16| ?? "<<endl;
+//    cin >> answers[1];
+//    cout <<" The Third Sequence "<<endl;
+//    cout <<" 1 | 5 | 10 | 16| ?? "<<endl;
+//    cin >> answers[2];
+//    int Sequence [3][5] = {
+//      {1 ,2 ,3 ,5 ,8 },
+//      {2 ,4, 8, 16,32}, 
+//      {1 ,5, 10, 16, 23}};
 
-   cout <<" The Third Sequence "<<endl;
-   cout <<" 1 | 5 | 10 | 16| ?? "<<endl;
-   cin >> answers[2];
-
-   int Sequence [3][5] = {
-     {1 ,2 ,3 ,5 ,8 },
-     {2 ,4, 8, 16,32}, 
-     {1 ,5, 10, 16, 23}};
-
-   if (answers[0] == Sequence[0][4])
-   {
-    points++;
-   }
-   if (answers[1] == Sequence[1][4])
-   {
-    points++;
-   }
-   if (answers[2] == Sequence[2][4])
-   {
-    points++;
-   }
-
-  cout <<"Your Points IS "<<points++<<" From 3"<<endl;
+//    if (answers[0] == Sequence[0][4])
+//    {
+//     points++;
+//    }
+//    if (answers[1] == Sequence[1][4])
+//    {
+//     points++;
+//    }
+//    if (answers[2] == Sequence[2][4])
+//    {
+//     points++;
+//    }
+//   cout <<"Your Points IS "<<points++<<" From 3"<<endl;
+//   cout <<" The Answers is "<<  Sequence[0][4]<<","<<Sequence[1][4]<<","<<Sequence[2][4]<<endl;
+return 0;
+}
 
 
-  cout <<" The Answers is "<<  Sequence[0][4]<<","<<Sequence[1][4]<<","<<Sequence[2][4]<<endl;
-
-
-    return 0;
-    }
